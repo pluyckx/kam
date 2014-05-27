fe52a8dcd37c3a46a754f764d660ba00e35b57c0
Philip Luyckx <philip.luyckx@gmail.com>
2014-05-27 22:39:42 +0200

    Add checks for necessary packages
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- install.sh
+++ install.sh
@@ -8,8 +8,52 @@ then
 	exit 1
 fi
 
+echo "Checking if necessary packages are installed"
+dpkg -S python3 2>&1 > /dev/null
+if [[ $? -eq 0 ]]
+then
+	python_available=1
+else
+	python_available=0
+fi
+
+dpkg -S python3-psutil 2>&1 > /dev/null
+if [[ $? -eq 0 ]]
+then
+	psutil_available=1
+else
+	psutil_available=0
+fi
+
+if [[ $python_available -eq 0 || $psutil_available -eq 0 ]]
+then
+	echo "Installing necessary packages"
+
+	packages=""
+
+	if [[ $python_available -eq 0 ]]
+	then
+		echo "Installing python3"
+		packages="$packages python3"
+	fi
+
+	if [[ $psutils_available -eq 0 ]]
+	then
+		echo "Installing psutil for python3"
+		packages="$packages python3-psutil"
+	fi
+
+	apt-get install $packages
+fi
+
+echo "Installing monitor.py to /usr/bin/kamd"
 cp monitor.py /usr/sbin/kamd
+
+echo "Installing init script to /etc/init.d"
 cp kam /etc/init.d/
 
+echo "Update rc.d"
 update-rc.d kam defaults
+
+echo "Start service"
 service kam start
