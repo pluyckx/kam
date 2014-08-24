e2bc49885ed89f8380b7e76f0bb3a3c1f2cb24e1
Philip Luyckx <philip.luyckx@gmail.com>
2014-08-24 10:15:55 +0200

    Fix bugs
     * Log to logmanager
     * Update periodsleep last_run time at the correct moment
    
    Add:
     * Log when a check is called
    
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- kam_src/modules/plugins/core/periodsleep.py
+++ kam_src/modules/plugins/core/periodsleep.py
@@ -18,7 +18,6 @@ class PeriodSleep(CoreBase):
 	def _execute(self):
 		now = time.clock_gettime(time.CLOCK_MONOTONIC)
 		delta = now - self._last_run
-		self._last_run = now
 		time_to_sleep = self._sleep - delta
 
 		if self._debug:
@@ -27,6 +26,8 @@ class PeriodSleep(CoreBase):
 
 		if time_to_sleep > 0:
 			time.sleep(time_to_sleep)
+
+		self._last_run = time.clock_gettime(time.CLOCK_MONOTONIC)
 
 	def loadConfig(self, config):
 		err_value = ""
