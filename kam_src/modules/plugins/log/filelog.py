703397b06516e6aa3c374cd113c2300241f98ca3
Philip Luyckx <philip.luyckx@gmail.com>
2014-08-26 20:36:40 +0200

    Update default log file values
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- kam_src/modules/plugins/log/filelog.py
+++ kam_src/modules/plugins/log/filelog.py
@@ -71,13 +71,13 @@ class FileLog(Logger):
 		if path == None:
 			path = "/var/log/kam.log"
 		if max_lines == None:
-			max_lines = 0
+			max_lines = 200
 
 		self._path = path
 		try:
 			self._max_lines = int(max_lines)
 		except ValueError as ex:
-			self._max_lines = 0
+			self._max_lines = 200
 
 			syslog.syslog("[Kam-FileLog] Failed to parse max_lines from {0}; ValueError: {1}\n".format(max_lines, str(ex)))
 		except TypeError as ex:
