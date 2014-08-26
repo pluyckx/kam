260b5a12feaa97f7e81dd6da3ee2bc1a4bde465e
Philip Luyckx <philip.luyckx@gmail.com>
2014-08-26 21:51:19 +0200

    Update default values for core plugins and only execute idle command when checks are enabled
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- kam_src/modules/plugins/core/periodsleep.py
+++ kam_src/modules/plugins/core/periodsleep.py
@@ -19,10 +19,6 @@ class PeriodSleep(CoreBase):
 		delta = now - self._last_run
 		time_to_sleep = self._sleep - delta
 
-		if self._debug:
-			self._debug.log(self._debug.TYPE_EXECUTE, self,\
-			            "time_to_sleep", time_to_sleep, "", "")
-
 		if time_to_sleep > 0:
 			time.sleep(time_to_sleep)
 
@@ -34,32 +30,34 @@ class PeriodSleep(CoreBase):
 		if section:
 			try:
 				sleep = int(section.get(self.CONFIG_ITEM_PERIOD))
-			except Exception as e:
+			except ValueError as e:
 				err_value = str(e)
 				sleep = 0
 		else:
 			sleep = None
 
-		if sleep != None and sleep > 0:
+		if sleep != None and sleep > 10:
 			self._sleep = sleep
 			self._enable()
 		else:
-			self._disable()
+			sleep = 0
+			self._sleep = 10
+			err_value += ";No sleep value entered or sleep < 10, using default sleep period of 10 seconds"
 
 		if self._log:
 			if sleep == None:
 				self._log.log(self,\
-				              "No section [global] found to load the period from!")
+				              "No section [global] found to load the period from! Using default of 10 for sleep!")
 			elif sleep == 0:
 				self._log.log(self,\
-				              "The config contains an invalid vlaue for [global] -> period. Please enter an integer larger than 0")
+				              "The config contains an invalid vlaue for [global] -> period. Please enter an integer larger than 0, default sleep value of 10 used!")
 			else:
 				self._log.log(self,\
 				              "Config loaded, period time = {0} seconds".format(self._sleep))
 
 		if self._debug:
 			self._debug.log(self._debug.TYPE_CONFIG, self,\
-					self.CONFIG_ITEM_PERIOD, sleep,\
+					self.CONFIG_ITEM_PERIOD, self._sleep,\
 					err_value, self.isEnabled())
 
 def createInstance(data_dict):
