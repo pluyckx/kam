88810f181afbd9011d810645f5a882618169fffc
Philip Luyckx <philip.luyckx@gmail.com>
2014-09-20 15:56:35 +0200

    Add missing commas
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup,  find_packages
 
 setup(name="kam",
-      version="1.0.0"
-      description="Keep the machine alive on activity"
-      url="http://github.com/pluyckx/kam"
-      author="Philip Luyckx"
-      author_email="philip.luyckx+kam@gmail.com"
+      version="1.0.0",
+      description="Keep the machine alive on activity",
+      url="http://github.com/pluyckx/kam",
+      author="Philip Luyckx",
+      author_email="philip.luyckx+kam@gmail.com",
       license="GPLV2",
       packages=find_packages(),
       zip_safe=False,
