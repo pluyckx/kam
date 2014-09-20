4cc00db56c7801213511a24615d88a2a231f18df
Philip Luyckx <philip.luyckx@gmail.com>
2014-09-20 15:53:42 +0200

    Use setup to install kam
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from setuptools import setup,  find_packages
+
+setup(name="kam",
+      version="1.0.0"
+      description="Keep the machine alive on activity"
+      url="http://github.com/pluyckx/kam"
+      author="Philip Luyckx"
+      author_email="philip.luyckx+kam@gmail.com"
+      license="GPLV2",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requeres=['psutils>=1.2.1']
+)
