e5dfd6bd9e03f9fbc7ecf54e934668f6d8b99377
Philip Luyckx <philip.luyckx@gmail.com>
2014-10-25 18:11:03 +0200

    Add the keyboard check plugin
     * Update the config file with the keyboard section
     * Add a pollmanager which the keyboard plugin will use to detect keyboard
       input. We create a manager, so the next plugin (the mouse plugin) can
       use it too.
     * Add the keyboard plugin itself
     * Rework the main script a bit. The pollmanager uses its own thread,
       so when forking, we got trouble because the startup script will not
       exit.
       The new flow of code is: start the main script and create some global
       variables, fork, execute main, load all modules, start monitoring.
       In the previous versions the modules were loaded outside the main
       function.
    
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

---
+++ kam/modules/plugins/checks/keyboard.py
@@ -0,0 +1,132 @@
+##\package keyboard
+# \brief This plugin checks if there is keyboard input. If so, the machine is kept alive.
+#
+# In the config file you can create a sesction [keyboard].
+# Here you can define a comma separated list of keyboard input locations: \e keyboards, which the plugin must monitor.
+# It is also possible to let the plugin find out the keyboad input event in /dev/input, if it can autodetect your keyboads: great,
+# if not, you need to find the input event yourself and add it to the list. You can also use auto detection + explicit define
+# a path to a keyboad input.
+#
+# \author Philip Luyckx
+# \copyright GNU Public License
+
+# This file is part of Keep Alive Monitor (kam).
+#
+# Keep Alive Monitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Keep Alive Monitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Keep Alive Monitor.  If not, see <http://www.gnu.org/licenses/>.
+
+from kam.modules.plugins.checks.basecheck import BaseCheck
+
+import os
+
+class KeyboardCheck(BaseCheck):
+	CONFIG_NAME = "keyboard"
+	CONFIG_ITEM_KEYBOARDS = "keyboards"
+
+	def __init__(self, data_dict):
+		super().__init__()
+		self._debug = data_dict["debug"]
+		self._log = data_dict["log"]
+		self._pollmanager = data_dict["pollmanager"]
+		self._files = []
+
+		if self._debug:
+			self._read_from = []
+
+	def isAlive(self):
+		alive = super().isAlive()
+		self._dead()
+		return alive
+
+	def _run(self):
+		if self._debug:
+			self._debug.log(self._debug.TYPE_CHECK, self, "read from: ", self._read_from, "", super().isAlive())
+			self._read_from.clear()
+
+	def _keyboardActive(self, fd, event):
+		self._alive()
+		for f in self._files:
+			if f.fileno() == fd:
+				while self._pollmanager.hasInput(f.fileno()):
+					buf = f.read1(100)
+
+				if self._debug and not f.name in self._read_from:
+					self._read_from.append(f.name)
+
+	def loadConfig(self, config):
+		self._keyboards = []
+		err_value = ""
+
+		try:
+			section = config[self.CONFIG_NAME]
+		except KeyError as e:
+			section = None
+			err_value = e
+			s_keyboards = None
+
+		if section:
+			s_keyboards = section.get(self.CONFIG_ITEM_KEYBOARDS)
+		else:
+			s_keyboards = None
+		
+		if s_keyboards:
+			a_keyboards = s_keyboards.split(",")
+			for s_keyboard in a_keyboards:
+				s_keyboard = s_keyboard.strip()
+				if s_keyboard == "auto":
+					a_keyboard = self._findKeyboards()
+				else:
+					a_keyboard = [ s_keyboard ]
+
+				for keyboard in a_keyboard:
+					if not keyboard in self._keyboards:
+						self._keyboards.append(keyboard)
+
+		if len(self._keyboards) > 0:
+			self._enable()
+			for keyboard in self._keyboards:
+				input_path = "/dev/input/{0}".format(keyboard)
+				f = open(input_path, "rb")
+				self._files.append(f)
+				self._pollmanager.add(f.fileno(), self._keyboardActive)
+		else:
+			self._disable()
+
+		if self._debug:
+			self._debug.log(self._debug.TYPE_CONFIG, self, self.CONFIG_ITEM_KEYBOARDS,\
+			                self._keyboards, err_value, self.isEnabled())
+
+		if self._log:
+			self._log.log(self, "Config loaded: enabled={0}; keyboards={1}\n".format(self.isEnabled(), self._keyboards))
+
+	def _findKeyboards(self):
+		keyboards = []
+		sys_path = "/sys/class/input"
+		for event in os.listdir(sys_path):
+			event_path = os.path.join(sys_path, event)
+			device_path = os.path.join(event_path, "device")
+			if os.path.exists(device_path):
+				name_path = os.path.join(device_path, "name")
+				if os.path.exists(name_path):
+					with open(name_path, "r") as name_file:
+						name = name_file.read().lower()
+
+					if "keyboard" in name or "keyboard" in name_path:
+						keyboards.append(event)
+
+		return keyboards
+
+
+def createInstance(data_dict):
+	return KeyboardCheck(data_dict)
+
---
+++ kam/utils/pollmanager.py
@@ -0,0 +1,100 @@
+##\package pollmanager
+# \brief This utility class can be used to poll files using a thread so they are "non-blocking".
+#
+# \author Philip Luyckx
+# \copyright GNU Public License
+
+# This file is part of Keep Alive Monitor (kam).
+#
+# Keep Alive Monitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Keep Alive Monitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Keep Alive Monitor.  If not, see <http://www.gnu.org/licenses/>.
+
+from threading import Thread, Lock
+import select
+
+class PollManager(Thread):
+	def __init__(self, data_dict):
+		super().__init__()
+		self._debug = data_dict["debug"]
+		self._log = data_dict["log"]
+
+		self._fds = []
+		self._fds_new = []
+		self._lock = Lock()
+		self._lock_poll = Lock()
+		self._stop_poll = True
+		self._poll = select.poll()
+
+	def add(self, fd, callback, event_mask = 1):
+		self._lock.acquire()
+		self._fds_new.append((fd, event_mask, callback))
+		self._lock.release()
+
+		if not self.isAlive() and self._stop_poll:
+			self._stop_poll = False
+			self.start()
+
+	def stop(self):
+		self._stop_poll = True
+
+	def run(self):
+		poll = self._poll
+
+		while not self._stop_poll and (len(self._fds) > 0 or len(self._fds_new) > 0):
+			self._lock.acquire()
+			while len(self._fds_new) > 0:
+				fd = self._fds_new.pop()
+				self._fds.append((fd[0], fd[1], fd[2]))
+				poll.register(fd[0], fd[1])
+			self._lock.release()
+
+			self._lock_poll.acquire()
+			ret = poll.poll(1000)
+			self._lock_poll.release()
+
+			if len(ret) > 0:
+				for (fd, event) in ret:
+					f = self._find_fd(fd)
+					if not f:
+						self._log.log(self, "fd {0} not found in registered list!".format(fd))
+					else:
+						f[2](fd, event)
+
+	def hasInput(self, fd):
+		self._lock_poll.acquire()
+		f = self._find_fd(fd)
+		if f:
+			self._poll.unregister(fd)
+
+		poll = select.poll()
+		poll.register(fd, select.POLLIN)
+		more_input = len(poll.poll(0)) == 1
+
+		poll.unregister(fd)
+		if f:
+			self._poll.register(f[0], f[1])
+		self._lock_poll.release()
+
+		return more_input
+	
+
+	def _find_fd(self, fd):
+		for f in self._fds:
+			if f[0] == fd:
+				return f
+
+		return None
+
+def createInstance(data_dict):
+	return PollManager(data_dict)
+
