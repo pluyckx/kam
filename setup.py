6b07a00878f1cd8e74a640bd22c9cec983aab5e5
Philip Luyckx <philip.luyckx@gmail.com>
2015-05-13 07:50:48 +0200

    Fixes https://github.com/pluyckx/kam/issues/15
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Command
 from distutils.command.install import install
 import subprocess
 
-class UpdateRc(install):
+class UpdateInit(install):
 	def run(self):
 		if os.path.exists("/etc/kam/kam.conf"):
 			shutil.copyfile("/etc/kam/kam.conf", "/etc/kam/kam.conf.back")
@@ -18,8 +18,14 @@ class UpdateRc(install):
 
 		super().run()
 
-		print("Update-rc.d")
-		subprocess.call(["update-rc.d", "kam", "defaults"])
+		if has_bin("update-rc.d"):
+			print("Update-rc.d")
+			subprocess.call(["update-rc.d", "kam", "defaults"])
+
+		if has_bin("systemctl"):
+			print("Update systemd")
+			subprocess.call(["systemctl", "enable", "kam.service"])
+
 
 def check_version():
 	(major, minor, _, _, _) = sys.version_info
@@ -55,6 +61,12 @@ def find_packages(relative_dir, packages, package_dir):
 			print("checking {0}".format(f))
 			find_packages(os.path.join(relative_dir, f), packages, package_dir)
 
+def has_bin(bin_name):
+	ret = subprocess.call(["locate", bin_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+	
+	return True if ret == 0 else False
+
+
 def main():
 	check_version()
 	check_psutil()
@@ -62,6 +74,8 @@ def main():
 	packages = []
 	package_dir = {}
 	find_packages("kam", packages, package_dir)
+
+	install = UpdateInit
 
 	setup(name="kam",
 	      version="1.0.0",
@@ -75,9 +89,10 @@ def main():
 	      scripts=["kam/bin/kamd"],
 	      data_files=[
 	                  ("/etc/kam", ["kam.conf", "version"]),
-	                  ("/etc/init.d", ["kam/init/kam"])
+	                  ("/etc/init.d", ["kam/init/kam"]),
+	                  ("/lib/systemd/system", ["kam/init/kam.service"])
 	      ],
-	      cmdclass = { 'install': UpdateRc }
+	      cmdclass = { 'install': install }
 	)
 
 if __name__ == "__main__":
