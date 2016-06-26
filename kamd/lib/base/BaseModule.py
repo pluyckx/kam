8934871bb8e4c366a26fd15680e20152e514b3d8
pluyckx <philip.luyckx@gmail.com>
2016-06-26 20:52:30 +0200

    Preparation for kam v2.0.0 directory layout
    Signed-off-by: pluyckx <philip.luyckx@gmail.com>

---
+++ kamd/lib/base/BaseModule.py
@@ -0,0 +1,5 @@
+
+
+class BaseModule(object):
+	def __init__(self):
+		
---
+++ kamd/lib/logging/Logging.py
@@ -0,0 +1,20 @@
+
+
+class Logging(object):
+	NONE = 0
+	ERROR = 1
+	WARNING = 2
+	INFO = 3
+	DEBUG = 4
+
+	def __init__(self, fileLogLevel=Logging.INFO, \
+	                   stdoutLogLevel=Logging.NONE):
+		self._fileLogLevel = fileLogLevel
+		self._stdoutLogLevel = stdoutLogLevel
+
+	def debug(self, obj, msg):
+		out = "[DEBUG] " + obj.__name__ + ": " + msg
+
+		if self._fileLogLevel:
+			
+
