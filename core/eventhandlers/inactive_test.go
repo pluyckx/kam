ae256840fec61556df0bd1136642ae167905f634
Philip Luyckx <philip.luyckx@gmail.com>
2018-02-12 20:01:36 +0100

    Add unit test file to test inactive.go
    Signed-off-by: Philip Luyckx <philip.luyckx@gmail.com>

---
+++ core/eventhandlers/inactive_test.go
@@ -0,0 +1,53 @@
+package eventhandlers
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pluyckx/kam/logging"
+
+	"github.com/pluyckx/kam/config"
+)
+
+const enableLogger = true
+
+const inactiveFullConfig = `
+[eventhandler]
+
+	[eventhandler.inactivetimeout]
+
+		[eventhandler.inactivetimeout.command]
+			command = "echo"
+			parameters = ["Computer", "is idle"]
+`
+
+const eventhandlerKey = "eventhandler"
+const inactiveTimeoutKey = "inactivetimeout"
+const commandKey = "command"
+
+func TestLoadFullConfig(t *testing.T) {
+	if enableLogger {
+		logging.RegisterLogger("", logging.NewLogger(os.Stdout))
+		logging.GetLogger("").SetLevel(logging.Level_Debug)
+	}
+
+	cfg, err := config.LoadData(inactiveFullConfig)
+
+	if err != nil {
+		t.Error("Invalid config")
+		return
+	}
+
+	cfg = cfg.Section("eventhandler")
+
+	if cfg == nil {
+		t.Error("eventhandler section not found")
+		return
+	}
+
+	cmd := InactiveTimeoutCommand{}
+
+	if !cmd.LoadConfig(cfg) {
+		t.Error("Problems during loading config")
+	}
+}
